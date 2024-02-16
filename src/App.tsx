43e168e76d82b47e6ffe79ee5c97339bf3e4b843
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-02-16 17:34:33 -0600

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import QRCode from "react-qr-code";
+import { useState } from "react";
 
 function App() {
-  
-
+  const [qrText, setQrText] = useState<string | null>(null);
+  const handleChange = (e: React.BaseSyntheticEvent) => {
+    setQrText(e.target.value);
+  };
   return (
     <div className="min-h-screen bg-gradient-to-b from-lime-100 to-purple-300 text-center text-purple-500">
       <h1 className="text-6xl">QR Code</h1>
       <div className="mt-16 flex flex-col items-center justify-center gap-4">
-
-        <div className="flex aspect-square w-64 items-center justify-center border-2 border-solid border-purple-500">
-          <div >Nothing</div>
-        </div>
+      {qrText ? (
+          <QRCode value={qrText} />
+        ) : (
+          // This was the original code
+          <div className="flex aspect-square w-64 items-center justify-center border-2 border-solid border-purple-500">
+            <div className="">Nothing</div>
+          </div>
+        )}
         
         <label htmlFor="qrValue">Write a text to convert it to QR code</label>
         <input
@@ -17,6 +25,7 @@ function App() {
           type="text"
           name="qrValue"
           id="qrValue"
+          onChange={handleChange}
         />
       </div>
     </div>
